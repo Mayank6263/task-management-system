4f89030b024d2de137069a4a279c15d195dc5a60
Mayank <75mayanksolanki@gmail.com>
2025-10-18 23:40:17 +0530

    TMS-01 Added devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_17_103028) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_18_180229) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
-
-  create_table "profiles", force: :cascade do |t|
-    t.string "firstname"
-    t.string "lastname"
-    t.integer "age"
-    t.integer "gender", default: 0
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,6 +25,4 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_17_103028) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_foreign_key "profiles", "users"
 end
